81055c146614429f9fe5630812469f2f3ec8d863
“Huma <humabhutti>
2023-10-04 16:25:01 +0500

    chapter-1

---
+++ app.js
@@ -0,0 +1,18 @@
+// Practice exercise 1.3 
+console.log("Hello world")
+
+// Practice exercise 1.4 
+
+let a = 10;
+console.log("The value of 'a' is:", a)
+
+  /*This JavaScript file is a basic example that demonstrates
+        variable declaration and logging to the console. It sets
+        a variable 'a' to the value 10 and logs its value.*/
+        
+        // Chapter project
+
+        
+        // This is a multi-line comment.
+// You can add your name and description here.
+// console.log("Your Name"); // Commented out to prevent console output
